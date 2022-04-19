d506c8d9d7b1119312395311bbce1fbac1d5c1c8
rookie-academy <newsubmission.ra@gmail.com>
2022-04-19 13:35:54 +0800

    ddd

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,13 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/css/');
     eleventyConfig.addPassthroughCopy('./src/assets/');
+
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+      });
+
     return {
         dir: {
             input:"src",
