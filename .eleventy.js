1ecc37e28048a27243d5fd10e41f0ca925c2db70
rookie-academy <newsubmission.ra@gmail.com>
2022-04-21 10:43:42 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -4,15 +4,20 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/css/');
     eleventyConfig.addPassthroughCopy('./src/assets/');
+
     eleventyConfig.addPassthroughCopy('./src/android-chrome-192x192.png');
     eleventyConfig.addPassthroughCopy('./src/android-chrome-512x512.png');
     eleventyConfig.addPassthroughCopy('./src/apple-touch-icon.png');
     eleventyConfig.addPassthroughCopy('./src/favicon-16x16.png');
     eleventyConfig.addPassthroughCopy('./src/favicon-32x32.png');
     eleventyConfig.addPassthroughCopy('./src/favicon.ico');
-    eleventyConfig.addPassthroughCopy('./src/robots.txt');
     eleventyConfig.addPassthroughCopy('./src/site.webmanifest');
+
+    eleventyConfig.addPassthroughCopy('./src/robots.txt');
+
     eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
+
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
