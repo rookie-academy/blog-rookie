15fb2e7026db3cee5d2885a8b188ebcb9268b256
rookie-academy <newsubmission.ra@gmail.com>
2022-04-25 11:18:33 +0800

    site map

--- .eleventy.js
+++ .eleventy.js
@@ -15,10 +15,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/favicon.ico');
     eleventyConfig.addPassthroughCopy('./src/site.webmanifest');
 
-    eleventyConfig.addPassthroughCopy('./src/robots.txt');
-
-    eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
-
     eleventyConfig.addPassthroughCopy('./src/admin');
 
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
